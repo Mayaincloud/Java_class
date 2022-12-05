cb2466de2e1820efceb016cbe911094e5ef125ad
Olga M <89043833391@mail.ru>
2022-12-05 16:28:39 +0500

    add Seminar1

---
+++ Seminar_1/Task1.java
@@ -0,0 +1,22 @@
+package Seminar_1;
+
+// Текст задачи:
+// Написать программу, которая запросит пользователя ввести <Имя> в 
+// консоли. Получит введенную строку и выведет в консоль сообщение “Привет, 
+// <Имя>!”
+
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.print(("Input name: "));
+        Scanner iScanner = new Scanner(System.in);
+        String name = iScanner.nextLine();
+        System.out.printf(("Привет, %s\n"), name);
+        name = "Привет, " + name;
+        System.out.println(name);
+        iScanner.close();
+    }
+  
+}
---
+++ Seminar_1/program.java
@@ -0,0 +1,17 @@
+package Seminar_1;
+
+// **Текст задачи:**
+//  Настроить проект, вывести в консоль “Hello world!”.
+
+// Вывести в консоль системные дату и время.
+
+import java.time.LocalDateTime;
+
+public class program {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        LocalDateTime nowTime = LocalDateTime.now();
+        System.out.println(nowTime);
+    }
+}
