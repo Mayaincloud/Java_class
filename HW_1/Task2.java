e99380f922abfab2de2383f33eefa992e4e60a9b
Olga M <89043833391@mail.ru>
2022-12-05 16:41:19 +0500

    add HW_1

---
+++ HW_1/Task2.java
@@ -0,0 +1,21 @@
+package HW_1;
+
+// Вывести все простые числа от 1 до 1000
+
+
+public class Task2 {
+    public static void main(String[] args) {
+        int number = 1;
+        int count = 0;
+
+        for (int j = 0; j < 1000; j++) {
+            number++;
+            for (int i = 1; i < number; i++) {
+                if (number % i == 0) count++; 
+            }
+            if (count < 2) System.out.printf("%d ", number);
+            count = 0; 
+        }
+    }
+    
+}
